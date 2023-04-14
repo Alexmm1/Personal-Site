b374fed098e4f6d407ec57faee877c74045b2e9e
Alexander <alexmayeux64@gmail.com>
2023-04-14 06:49:52 -0500

    Add DriveEze to Portfolio as a Third Project

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -4,16 +4,21 @@ const Portfolio = () => {
     return(
     <div>
         <main>
-            <h1>Things I've built, in no particular order</h1>
+            <h1>Here are some of the projects I have developed, listed in no particular order:</h1>
             <section className="project">
                 <h2 className="project-name">The First Project: Juicebox</h2>
-                <p>Developing Juicebox was an excellent opportunity for me to hone my skills in both front-end and back-end web development. Specifically, I gained valuable experience in setting up databases and APIs, allowing me to work seamlessly on both ends of the development process.</p>
-                <p>Here is the github repository for Juicebox: https://github.com/Alexmm1/Juicebox </p>
+                <p>Developing Juicebox presented a unique opportunity to refine my expertise in both front-end and back-end web development, which proved to be invaluable. This project allowed me to acquire valuable experience in setting up databases and APIs, enabling me to work seamlessly on both ends of the development process.</p>
+                <p>You may access the Github repository for Juicebox at the following link: https://github.com/Alexmm1/Juicebox </p>
             </section>
             <section className="project">
                 <h2 className="project-name">The Second Project: FinessTrackr</h2>
-                <p>Developing FitnessTrackr with my school partner was an excellent opportunity for us to hone our skills in both front-end and back-end web development. Specifically, we gained valuable experience in setting up databases and APIs, allowing us to work seamlessly on both ends of the development process.</p>
-                <p>Here is the github repository for FinessTrackr: https://github.com/Alexmm1/FinessTrackr</p>
+                <p>Collaborating with my school partner on the development of FitnessTrackr presented us with a valuable opportunity to enhance our expertise in both front-end and back-end web development. Through our focus on setting up databases and APIs, we acquired practical experience and knowledge that enabled us to work effortlessly on both ends of the development process. This experience proved to be instrumental in our successful completion of the project.</p>
+                <p>You may access the Github repository for FinessTrackr at the following link: https://github.com/Alexmm1/FinessTrackr</p>
+            </section>
+            <section className="project">
+                <h2 className="project-name">The Third Project: DriveEze</h2> 
+                <p>Collaborating with my school partner on the development of DrivEze was a crucial step in refining our expertise in both front-end and back-end web development. Our emphasis on setting up databases and APIs allowed us to work seamlessly on both ends of the development process, ultimately resulting in a successful outcome. This invaluable experience has equipped us with the necessary skills to tackle complex web development challenges in our future business endeavors.</p>
+                <p>You may access the Github repository for DriveEze at the following link: https://github.com/TheHighnote/DrivEze-Backend https://github.com/vvazquez307/DrivEze</p>
             </section>
         </main>
     </div>
